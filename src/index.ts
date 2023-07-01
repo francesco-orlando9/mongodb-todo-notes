c20454b67d5e74bc0cfd162f6da2de8a1b79b6ba
Francesco Orlando <francesco.orlando.dev@gmail.com>
2023-07-01 18:17:45 +0200

    add index + package json + tsconfig

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import fastify from 'fastify';
+import mongoose from 'mongoose';
+
+import CONFIG from './config/config';
+import userRoutes from './api/v1/routes/user';
+
+const server = fastify({ logger: true });
+
+mongoose.connect(CONFIG.MONGO.URL).then(() => {
+    console.log('Connected to MongoDb')
+    server.listen({ port: CONFIG.SERVER.PORT, host: CONFIG.SERVER.ADDRESS }, (err, address) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
+    
+        console.log(`Server listening at ${address}`);
+    });
+}).catch((error) => {
+    console.log(error)
+})
+
+
+server.get('/', async (request, reply) => {
+    reply.send({
+        status: true
+    })
+});
+
+server.register(userRoutes, { prefix: '/user'});
